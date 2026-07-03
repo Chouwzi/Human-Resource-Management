7a2c2a094023401081eda57f810e392c162e611c
Kim Long <kimlong210806@gmail.com>
2026-07-03 10:44:50 +0700

    Đã thêm reponsive vào file admin và components Cập nhật thêm class form-control vào các thẻ input

--- resources/views/leaves/employee.blade.php
+++ resources/views/leaves/employee.blade.php
@@ -69,7 +69,7 @@
                             
                             <div>
                                 <label class="block text-sm font-semibold text-slate-700 mb-1">Thời gian nghỉ (Chi tiết)</label>
-                                <input type="text" id="leave-duration" placeholder="VD: 25/06 - 27/06 (3 ngày)" class="w-full border border-slate-200 rounded-lg px-4 py-2.5 text-sm focus:ring-2 focus:ring-teal-500 focus:border-teal-500 outline-none transition" required>
+                                <input type="text" id="leave-duration" class="form-control" placeholder="VD: 25/06 - 27/06 (3 ngày)" class="w-full border border-slate-200 rounded-lg px-4 py-2.5 text-sm focus:ring-2 focus:ring-teal-500 focus:border-teal-500 outline-none transition" required>
                             </div>
 
                             <div>
