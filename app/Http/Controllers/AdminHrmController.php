f2d5e7c4d5d47f5e343e914cb05acc16dd8adb66
Nguyễn Trần Đình Chương <ntd.chuong@gmail.com>
2026-07-10 01:47:14 +0700

    fix(attendance): remove redundant manual check-out validation

--- app/Http/Controllers/AdminHrmController.php
+++ app/Http/Controllers/AdminHrmController.php
@@ -199,16 +199,6 @@ class AdminHrmController extends Controller
     {
         $data = $request->validate($this->attendanceRules(), $this->validationMessages(), $this->validationAttributes());
 
-        if ($request->filled('check_in_at') && $request->filled('check_out_at')) {
-            $checkIn  = \Carbon\Carbon::parse($request->work_date . ' ' . $request->check_in_at);
-            $checkOut = \Carbon\Carbon::parse($request->work_date . ' ' . $request->check_out_at);
-            if ($checkOut->lte($checkIn)) {
-                return redirect()->back()
-                    ->withInput()
-                    ->with('error', 'Giờ check-out phải sau giờ check-in.');
-            }
-        }
-
         $data = $this->calculateAttendance($data);
 
         AttendanceLog::updateOrCreate(
